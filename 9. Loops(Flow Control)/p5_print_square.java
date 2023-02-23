f3556c95749c627c23113abd9eb37846e061f8b2
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-23 17:22:24 +0530

    Print Square using for loop

--- 9. Loops(Flow Control)/p5_print_square.java
+++ 9. Loops(Flow Control)/p5_print_square.java
@@ -1,6 +1,8 @@
 public class p5_print_square {
     public static void main(String args[]){
 
+        // this code is writen by Priyanshu Gour
+
 
         for(int i = 1 ; 5 >= i ; i++){
             System.out.println("* * * * *");
