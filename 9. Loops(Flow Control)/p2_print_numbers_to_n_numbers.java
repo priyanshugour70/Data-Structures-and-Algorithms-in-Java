0903fdbf076e898b85c1d800f2992b9c606dde2b
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-23 17:21:03 +0530

    Print nubmers to n numbers

--- 9. Loops(Flow Control)/p2_print_numbers_to_n_numbers.java
+++ 9. Loops(Flow Control)/p2_print_numbers_to_n_numbers.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class p2_print_numbers_to_n_numbers {
+
+    // this code is writen by Priyanshu Gour
+    
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
