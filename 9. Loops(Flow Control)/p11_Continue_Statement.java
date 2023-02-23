01597c42161b83b5fe39cd9160973d889b36822a
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-23 17:27:35 +0530

    Continue Statement

--- 9. Loops(Flow Control)/p11_Continue_Statement.java
+++ 9. Loops(Flow Control)/p11_Continue_Statement.java
@@ -1,6 +1,8 @@
 public class p11_Continue_Statement {
     public static void main(String args[]){
 
+        // this code is writen by Priyanshu Gour
+
         for(int i = 1 ; 10 >= i ; i++){
             if(i % 2 == 0){
                 continue;
