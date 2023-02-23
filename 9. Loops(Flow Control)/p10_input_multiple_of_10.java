18590547b5a1866807c19a9d78133a7935c90c07
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-02-23 17:27:09 +0530

    Input multiple of 10

--- 9. Loops(Flow Control)/p10_input_multiple_of_10.java
+++ 9. Loops(Flow Control)/p10_input_multiple_of_10.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class p10_input_multiple_of_10 {
+
+    // this code is writen by Priyanshu Gour
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
