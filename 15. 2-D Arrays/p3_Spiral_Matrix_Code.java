6659842dbc683170e84e36185a8b8e463cdac2c1
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-24 22:35:56 +0530

    Spiral Matrix Code in java

--- 15. 2-D Arrays/p3_Spiral_Matrix_Code.java
+++ 15. 2-D Arrays/p3_Spiral_Matrix_Code.java
@@ -1,11 +1,52 @@
 public class p3_Spiral_Matrix_Code {
 
-    // this code is writen by Priyanshu Gour
+    // this code is written by Priyanshu Gour
+
+    public static void spiralMatrix(int matrix [][]){
+
+        int startRow = 0 ;
+        int startCol = 0 ;
+        int endRow = matrix.length-1 ;
+        int endCol = matrix.length-1 ;
+
+
+        while(startRow <= endRow && startCol <= endCol){
+
+            // Top
+            for(int j = startCol ; j <= endCol ;j++){
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            // Right
+            for(int i = startRow + 1 ; i <= endRow ; i++){
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            // Bottom 
+            for(int j = endCol -1 ; j >= startCol ; j--){
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            // Left
+            for(int i = endRow-1 ; i >= startRow +1 ;i--){
+                System.out.print(matrix[i][startCol] + " ");
+            }
+
+            startCol++ ;
+            startRow++ ;
+            endCol-- ;
+            endRow-- ;
+        }
+    }
     
 
     public static void main(String args[]){
-        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int matrix[][] = {{1,2,3,4},
+                         {5,6,7,8},
+                         {9,10,11,12},
+                         {13,14,15,16}};
 
+        spiralMatrix(matrix);
         
     }
 }
