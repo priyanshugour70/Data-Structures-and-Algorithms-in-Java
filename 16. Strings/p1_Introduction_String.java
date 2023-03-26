88c69e40b7360b8f779914ed7d2e41a9106a4a50
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-26 22:52:03 +0530

    Introduction to Strings in java

---
+++ 16. Strings/p1_Introduction_String.java
@@ -0,0 +1,13 @@
+public class p1_Introduction_String {
+    
+    public static void main(String[] args) {
+        
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "Priyanshu Gour" ;
+        String str2 = new String("Priyanshu Gour");
+
+        System.out.println(arr[0]);
+        System.out.println(str);
+        System.out.println(str2);
+    }
+}
