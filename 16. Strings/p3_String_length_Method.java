6f6597b08bf51ded806b61732914adcce02682d4
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-26 23:13:08 +0530

    String length Method in java

---
+++ 16. Strings/p3_String_length_Method.java
@@ -0,0 +1,11 @@
+public class p3_String_length_Method {
+    
+    public static void main(String[] args) {
+        
+        String fullName = "Priyanshu Gour" ;
+
+        int len = fullName.length();
+        
+        System.out.println(len);
+    }
+}
