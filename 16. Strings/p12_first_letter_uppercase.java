722abb246e8c4ff61c460b860cb00e9a62164bb3
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-27 23:59:51 +0530

    First letter uppercase in java

---
+++ 16. Strings/p12_first_letter_uppercase.java
@@ -0,0 +1,31 @@
+public class p12_first_letter_uppercase {
+    
+    public static String toUpperCase(String str){
+
+        StringBuilder sb = new StringBuilder("");
+        
+        char ch = Character.toUpperCase(str.charAt(0)) ;
+        sb.append(ch);
+
+        for(int i = 1 ; i < str.length(); i++){
+            if(str.charAt(i) == ' ' && i < str.length()){
+                sb.append(str.charAt(i));
+                i++ ;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString() ;
+    }
+
+    public static void main(String[] args) {
+        
+        String str = "hey i am devil .. ";
+
+        str = toUpperCase(str); 
+
+        System.out.println(str);
+    }
+}
