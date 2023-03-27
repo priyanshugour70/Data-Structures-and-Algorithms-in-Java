bf64d182ebbf2dccf41effaaede8d40d8a9922b3
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-27 23:43:45 +0530

    StringBuilder mutable String in java

---
+++ 16. Strings/p11_StringBuilder_mutable_String.java
@@ -0,0 +1,13 @@
+public class p11_StringBuilder_mutable_String {
+    
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("hey buddy ");
+
+        sb.append("Priyanshu ") ;
+        sb.append("Gour");
+
+        System.out.println(sb);
+
+    }
+}
