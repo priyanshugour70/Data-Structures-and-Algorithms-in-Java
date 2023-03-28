400feff36bb22183d413d410cb93184182d69e82
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-28 21:48:22 +0530

    Introduction to Bitwise Operator in java

---
+++ 17. Bit Manipulation/p1_Introduction_to_Bitwise_Operator.java
@@ -0,0 +1,17 @@
+public class p1_Introduction_to_Bitwise_Operator {
+    
+    public static void main(String[] args) {
+        
+        // Bit manipulation is the process of manipulating individual bits of binary numbers to achieve some specific result. Java provides several operators and methods that enable programmers to perform bit manipulation easily.
+
+
+        // Bitwise AND (&): performs a bitwise AND operation on each bit of two integers, resulting in a new integer where each bit is set to 1 only if the corresponding bits of both integers are also 1.
+        // Bitwise OR (|): performs a bitwise OR operation on each bit of two integers, resulting in a new integer where each bit is set to 1 if the corresponding bit of either or both integers is 1.
+        // Bitwise XOR (^): performs a bitwise exclusive OR (XOR) operation on each bit of two integers, resulting in a new integer where each bit is set to 1 only if the corresponding bits of the two integers are different.
+        // Bitwise Complement (~): performs a bitwise complement operation on an integer, resulting in a new integer with all its bits inverted (0s become 1s and 1s become 0s).
+        // Signed Left Shift (<<): shifts the bits of an integer to the left by a specified number of positions. The leftmost bits are filled with 0s, and the rightmost bits are discarded.
+        // Signed Right Shift (>>): shifts the bits of an integer to the right by a specified number of positions. The rightmost bits are filled with the sign bit (the most significant bit), and the leftmost bits are filled with 0s.
+
+
+    }
+}
