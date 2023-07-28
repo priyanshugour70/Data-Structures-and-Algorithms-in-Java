ecef9e25a1be6b4794ba9e545772bfd77f33bd32
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-07-28 22:42:19 +0530

    2D Array

---
+++ 15. 2-D Arrays/p13_2DArray.java
@@ -0,0 +1,8 @@
+public class p13_2DArray {
+    public static void main(String[] args) {
+        int[][] a = new int[3][4];
+        System.out.println(a);// [[I@...
+        System.out.println(a[0]);// [I@..
+        System.out.println(a[0][0]);// 0
+    }
+}
